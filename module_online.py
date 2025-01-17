27f569e5a804818de9feb483a3c1ea301e71eec3
gizmore <gizmore@wechall.net>
2025-01-17 05:45:10 +0100

    'online users'

--- module_online.py
+++ module_online.py
@@ -1,5 +1,25 @@
 from gdo.base.GDO_Module import GDO_Module
+from gdo.base.GDT import GDT
+from gdo.core.GDT_Container import GDT_Container
+from gdo.online.GDT_OnlinePanel import GDT_OnlinePanel
+from gdo.ui.GDT_Page import GDT_Page
+from gdo.ui.GDT_PageLocation import GDT_PageLocation
 
 
 class module_online(GDO_Module):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self._priority = 40
+
+    def gdo_module_config(self) -> list[GDT]:
+        return [
+            GDT_PageLocation('online_bar_location').not_null().initial('_bottom_bar'),
+        ]
+
+    def cfg_page_location(self) -> GDT_Container:
+        return self.get_config_value('online_bar_location')
+
+    def gdo_init_sidebar(self, page: 'GDT_Page'):
+        location = self.cfg_page_location()
+        location.add_field(GDT_OnlinePanel())
