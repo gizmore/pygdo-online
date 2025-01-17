3580e1eb7159f7bdc0fef942d785b3d41e9a9c3c
Christian Busch <cb@oncrea.de>
2025-01-17 06:53:59 +0100

    'online users and fixes. even some events'

--- module_online.py
+++ module_online.py
@@ -1,5 +1,8 @@
+from gdo.base.Application import Application
+from gdo.base.Cache import Cache
 from gdo.base.GDO_Module import GDO_Module
 from gdo.base.GDT import GDT
+from gdo.core.GDO_User import GDO_User
 from gdo.core.GDT_Container import GDT_Container
 from gdo.online.GDT_OnlinePanel import GDT_OnlinePanel
 from gdo.ui.GDT_Page import GDT_Page
@@ -20,6 +23,22 @@ class module_online(GDO_Module):
     def cfg_page_location(self) -> GDT_Container:
         return self.get_config_value('online_bar_location')
 
+    def gdo_init(self):
+        Application.EVENTS.subscribe('user_setting_last_activity_changed', self.on_last_activity_changed)
+        Application.EVENTS.subscribe('user_logout', self.on_user_logout)
+
+    def on_last_activity_changed(self, user: GDO_User, val):
+        self.on_clear_cache()
+
+    def on_user_logout(self, user: GDO_User):
+        Cache.remove('online_users')
+
+    def on_clear_cache(self):
+        Cache.remove('online_users')
+
     def gdo_init_sidebar(self, page: 'GDT_Page'):
-        location = self.cfg_page_location()
-        location.add_field(GDT_OnlinePanel())
+        cached = Cache.get('online_users', 'all')
+        panel = cached or GDT_OnlinePanel()
+        if not cached:
+            Cache.set('online_users', 'all', panel)
+        self.cfg_page_location().add_field(panel)
