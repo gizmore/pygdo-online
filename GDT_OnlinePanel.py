93cc53178ed0107f989fe9a923be9901ab6740df
Christian Busch <cb@oncrea.de>
2025-01-22 20:25:33 +0100

    'translate args are tuples now'

--- GDT_OnlinePanel.py
+++ GDT_OnlinePanel.py
@@ -1,7 +1,5 @@
 from gdo.base.Trans import t
 from gdo.core.GDO_User import GDO_User
-from gdo.core.GDT_String import GDT_String
-from gdo.core.GDT_UInt import GDT_UInt
 from gdo.message.GDT_HTML import GDT_HTML
 from gdo.ui.GDT_Panel import GDT_Panel
 from gdo.user.GDT_ProfileLink import GDT_ProfileLink
@@ -15,4 +13,4 @@ class GDT_OnlinePanel(GDT_Panel):
         cut = module_user.instance().get_activity_cut_date()
         for user in GDO_User.table().with_setting_result([('last_activity', '>=', cut)]):
             self.add_field(GDT_ProfileLink().user(user))
-        self.get_fields().insert(0, GDT_HTML().text(t('online_users', [str(len(self._fields))])))
+        self.get_fields().insert(0, GDT_HTML().text(t('online_users', (str(len(self._fields)),))))
